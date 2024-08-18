4e0a45c578c6a9a3c95053cf8ed02e9b7fb8dab2
Mark Njenga <marknjenga25@gmail.com>
2024-08-18 11:26:01 +0300

    Done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ function Home() {
   const movieList = movies.map(movie => {
     return (
       <MovieCard key={movie.id} title={movie.title} id={movie.id}>
-        <Link to={`/movies/${movie.id}`}>View Info</Link>
+        <Link to={`/movies/${movie.id}`}>View Details</Link>
       </MovieCard>
     )
   })
